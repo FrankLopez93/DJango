1f1f3337fd42d24d48c37f97c086e4799011c960
FrankLopez93 <francisco.lopez666@hotmail.com>
2022-03-23 10:31:18 -0600

    new changes
    I added rute in file settings.py and I reduced code in views.py

--- firstProject/firstProject/settings.py
+++ firstProject/firstProject/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'firstProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['C:/Users/Fc/Desktop/projectsDjango/firstProject/firstProject/Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- firstProject/firstProject/views.py
+++ firstProject/firstProject/views.py
@@ -1,7 +1,7 @@
-#always import this library for views
-from django.http import HttpResponse
+from django.http import HttpResponse  #always import this library for views
 import datetime
 from django.template import Template, Context
+from django.template.loader import get_template
 
 class Persona(object):
     def __init__(self, nombre, apellido):
@@ -12,12 +12,20 @@ def gretting(request):
     p1 = Persona('Francisco', 'Lopez')
     #nombre = 'Javier'
     #apellido = 'Lopez'
+
+    temas = ['Plantillas', 'Modelos', 'Formularios', 'Vistas', 'Despliegue']
     ahora = datetime.datetime.now()
-    doc_externo = open("C:/Users/Fc/Desktop/projectsDjango/firstProject/firstProject/Templates/myTemplate.html")
-    templt = Template(doc_externo.read())
-    doc_externo.close()
-    ctx = Context({'nombre_persona':p1.nombre, 'apellido':p1.apellido, 'actual':ahora})
-    documento = templt.render(ctx)
+    # doc_externo = open("C:/Users/Fc/Desktop/projectsDjango/firstProject/firstProject/Templates/myTemplate.html")
+    # templt = Template(doc_externo.read())
+    # doc_externo.close()
+
+    doc_externo = get_template('myTemplate.html')
+    # ctx = Context({'nombre_persona':p1.nombre,'apellido':p1.apellido,'actual':ahora,'temas':temas})
+    documento = doc_externo.render({'nombre_persona':p1.nombre,
+                                    'apellido':p1.apellido,
+                                    'actual':ahora,
+                                    'temas':temas
+                                })
 
     return HttpResponse(documento)
 
